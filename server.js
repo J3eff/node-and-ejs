b599f74a90a4f34841c7c343cf579ace1eb395a7
J3eff <jeffersonvideo125@gmail.com>
2022-07-23 17:31:18 -0300

    Node + EJS

--- server.js
+++ server.js
@@ -4,7 +4,20 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function(req,res) {
-    res.render("pages/index");
+    const items = [
+        {title: "D", message: "esenvolver aplicações/serviços de forma facil"},
+        {title: "E", message: "JS usa JavaScript para renderizar HTML"},
+        {title: "M", message: "uito fácil de fazer debugging"},
+        {title: "A", message: "moriznho"},
+        {title: "I", message: "nstall ejs"},
+        {title: "S", message: "intaxe simples"},
+    ]
+
+    const subtitle = "Uma linguagem de modelagem para criação de paginas HTML utilizando JS"
+    res.render("pages/index", {
+        qualitys: items,
+        subtitle: subtitle
+    });
 });
 
 app.get("/sobre", function(req,res) {
